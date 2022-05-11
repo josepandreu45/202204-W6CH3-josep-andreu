55a907dde1805ca72e5212f9421c83cfc018f9e1
josep andreu <josepa45@gmail.com>
2022-05-11 17:50:00 +0200

    First Commit

---
+++ src/components/Display/Display.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import PhoneContext from "../../contexts/PhoneContext";
+
+
+const Display = () => {
+  const { phoneNumber } = useContext(PhoneContext);
+  return <span className="number">{phoneNumber}</span>;
+};
+
+export default Display;
